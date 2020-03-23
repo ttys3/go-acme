42d0ca22b569acda6c5974ab01c7f508c81b510a
荒野無燈 <ttys3@outlook.com>
2020-03-23 18:09:58 +0800

    test.sh: fixup typo

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ test -d ${ACME_ROOT_DIR} || mkdir -p ${ACME_ROOT_DIR}
 
 curl -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
      -H "Authorization: Bearer ${CF_DNS_API_TOKEN}" \
-     -H "Content-Type:application/json" \ &&
+     -H "Content-Type:application/json" && \
 env HTTP_PROXY=http://127.0.0.1:7070 \
 GO_ACME_KEY_PATH=${ACME_ROOT_DIR}/foo.key \
 GO_ACME_CERT_PATH=${ACME_ROOT_DIR}/foo.crt \
