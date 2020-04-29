96f67b22019adf175ad29a2e911bbd687d423181
荒野無燈 <ttys3@outlook.com>
2020-04-29 12:43:45 +0800

    fix: (cmd/auto-acme) set log level to warn if env var AUTOCERT_DNS_PROVIDER or AUTOCERT_DOMAIN empty

--- cmd/auto-acme/main.go
+++ cmd/auto-acme/main.go
@@ -60,7 +60,7 @@ func main() {
 	}
 	if _, updated, err := autocertManager(ctx, succCh, it); err != nil {
 		// just print the error message, do not exit, friendly to container
-		zap.S().Errorf("err: %s", err)
+		zap.S().Warn(err)
 	} else {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -130,75 +130,78 @@ func autocertManager(ctx context.Context, outSuccCh chan<- struct{}, it time.Dur
 	dnsprovider = strings.TrimSpace(dnsprovider)
 	tlsDomain := os.Getenv("AUTOCERT_DOMAIN")
 	tlsDomain = strings.TrimSpace(tlsDomain)
-	if tlsDomain != "" && dnsprovider != "" {
-		// enable SelfSigned if is it localhost or IP addr
-		useAcme := false
-		var providedDomains []string
-		var validDomains []string
-		if strings.Contains(tlsDomain, ",") {
-			providedDomains = strings.Split(tlsDomain, ",")
-		} else {
-			providedDomains = []string{tlsDomain}
-		}
-		for _, name := range providedDomains {
-			nameToCheck := name
-			// make wildcard name valid
-			if nameToCheck[:2] == "*." {
-				nameToCheck = nameToCheck[2:]
-			}
-			if IsDNSName(nameToCheck) {
-				validDomains = append(validDomains, name)
-			}
-		}
-		theDomain := &types.Domain{Main: "nodomain.localhost"}
-		if len(validDomains) > 0 {
-			useAcme = true
-			theDomain = &types.Domain{Main: validDomains[0], SANs: validDomains[1:]}
-		} else {
-			zap.S().Errorf("autocertManager(): ACME is disabled for reason: no valid domain provided")
-			zap.S().Infof("autocertManager(): fallback to selfsigned cert")
-		}
-		ACME := &acme.ACME{
-			BackendName: "fs", //cache path set by GO_ACME_STORAGE_DIR env
-			Email:       email,
-			KeyType:     keyType,
-			DNSProvider: dnsprovider,
-			SelfSigned:  !useAcme,
-			Domain:      theDomain,
-			KeyPath:     os.Getenv(keyPathEnv),
-			CertPath:    os.Getenv(certPathEnv),
-			CAServer:	 os.Getenv(caServerEnv),
-			Logger: 	 zap.NewStdLog(logger),
-		}
 
-		zap.S().Infof("autocertManager(): auto ACME begin, use ACME: %v, dns provider: %s, key type: %s",
-			useAcme, dnsprovider, keyType)
-		zap.S().Infof("autocertManager(): email: %s, domains: %+v", email, validDomains)
-
-		domainWhitelist := strings.TrimSpace(domainWhitelist)
-		if domainWhitelist != "" {
-			hostWhitelist := strings.Split(domainWhitelist, ",")
-			if len(hostWhitelist) > 0 {
-				ACME.HostPolicy = acme.HostWhitelist(hostWhitelist...)
-				zap.S().Infof("autocertManager(): host whitelist: %+v", hostWhitelist)
-			}
-		}
-		tlsConfig := &tls.Config{}
-		if isUpdated, err := ACME.CreateConfig(ctx, outSuccCh, it, tlsConfig); err != nil {
-			panic(err)
-		} else {
-			updated = isUpdated
-		}
-		zap.S().Infof("autocertManager(): auto ACME done")
-		return tlsConfig, updated, nil
-	} else {
+	if tlsDomain == "" || dnsprovider == "" {
 		return nil, updated, fmt.Errorf("autocertManager(): env var AUTOCERT_DNS_PROVIDER and AUTOCERT_DOMAIN must not be empty")
 	}
+
+	// enable SelfSigned if is it localhost or IP addr
+	useAcme := false
+	var providedDomains []string
+	var validDomains []string
+	if strings.Contains(tlsDomain, ",") {
+		providedDomains = strings.Split(tlsDomain, ",")
+	} else {
+		providedDomains = []string{tlsDomain}
+	}
+	for _, name := range providedDomains {
+		nameToCheck := name
+		// make wildcard name valid
+		if nameToCheck[:2] == "*." {
+			nameToCheck = nameToCheck[2:]
+		}
+		if IsDNSName(nameToCheck) {
+			validDomains = append(validDomains, name)
+		}
+	}
+	theDomain := &types.Domain{Main: "nodomain.localhost"}
+	if len(validDomains) > 0 {
+		useAcme = true
+		theDomain = &types.Domain{Main: validDomains[0], SANs: validDomains[1:]}
+	} else {
+		zap.S().Errorf("autocertManager(): ACME is disabled for reason: no valid domain provided")
+		zap.S().Infof("autocertManager(): fallback to selfsigned cert")
+	}
+	ACME := &acme.ACME{
+		BackendName: "fs", //cache path set by GO_ACME_STORAGE_DIR env
+		Email:       email,
+		KeyType:     keyType,
+		DNSProvider: dnsprovider,
+		SelfSigned:  !useAcme,
+		Domain:      theDomain,
+		KeyPath:     os.Getenv(keyPathEnv),
+		CertPath:    os.Getenv(certPathEnv),
+		CAServer:	 os.Getenv(caServerEnv),
+		Logger: 	 zap.NewStdLog(logger),
+	}
+
+	zap.S().Infof("autocertManager(): auto ACME begin, use ACME: %v, dns provider: %s, key type: %s",
+		useAcme, dnsprovider, keyType)
+	zap.S().Infof("autocertManager(): email: %s, domains: %+v", email, validDomains)
+
+	domainWhitelist = strings.TrimSpace(domainWhitelist)
+	if domainWhitelist != "" {
+		hostWhitelist := strings.Split(domainWhitelist, ",")
+		if len(hostWhitelist) > 0 {
+			ACME.HostPolicy = acme.HostWhitelist(hostWhitelist...)
+			zap.S().Infof("autocertManager(): host whitelist: %+v", hostWhitelist)
+		}
+	}
+	tlsConfig = &tls.Config{}
+	if isUpdated, err := ACME.CreateConfig(ctx, outSuccCh, it, tlsConfig); err != nil {
+		panic(err)
+	} else {
+		updated = isUpdated
+	}
+	zap.S().Infof("autocertManager(): auto ACME done")
+	return tlsConfig, updated, nil
 }
 
 func initLogger() func() {
 	zapCfg := zap.NewDevelopmentConfig()
 	zapCfg.DisableCaller = true
+	// if Development, stackLevel = WarnLevel, else ErrorLevel
+	zapCfg.Development = false
 	tmpLogger, _ := zapCfg.Build()
 	logger = tmpLogger.Named("[auto-acme]")
 	//The default global logger used by zap.L() and zap.S() is a no-op logger.
